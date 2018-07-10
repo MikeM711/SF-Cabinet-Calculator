f012a49126a433f13dda40cbc97cf58f90c0a97f
Michael <michaelmccabe711@gmail.com>
2018-07-10 16:26:12 -0400

    new edit 7/10

--- build/app.js
+++ build/app.js
@@ -16,9 +16,9 @@ app.controller('AppController', ['$http', function($http) {
 
   //Because I am currently using localhost, I use the localhost:3000/calculator as my apiRoute
 
-   //var apiRoute = 'http://localhost:3000/calculator';
+   var apiRoute = 'http://localhost:3000/calculator';
 
-   var apiRoute = 'https://ag-sf-calculator.herokuapp.com/calculator';
+   //var apiRoute = 'https://ag-sf-calculator.herokuapp.com/calculator';
 
 
 
